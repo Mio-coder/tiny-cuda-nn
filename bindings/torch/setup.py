b6ce4fcc0892ad1eef40c18d6d1b50ef1333d7cd
Mio-coder <78543744+Mio-coder@users.noreply.github.com>
2023-10-04 22:40:11 +0200

    Update setup.py

--- bindings/torch/setup.py
+++ bindings/torch/setup.py
@@ -2,7 +2,7 @@ import os
 
 import re
 from setuptools import setup
-from setuptools.pkg_resources import parse_version
+from setuptools.extern.packaging.version import parse as parse_version
 import subprocess
 import shutil
 import sys
